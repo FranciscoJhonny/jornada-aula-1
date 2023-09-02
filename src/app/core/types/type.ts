e4abae9146d5192c642bbcda3e0f1766fc784d5c
Francisco Jhonny <fmaia@fazenda.ms.gov.br>
2023-09-02 12:29:13 -0400

    Modeificação aula angular

---
+++ src/app/core/types/type.ts
@@ -0,0 +1,21 @@
+export interface Promocao {
+  id: number;
+  destino: string;
+  imagem: string;
+  preco: number;
+}
+export interface UnidadeFederativa {
+  id: number;
+  nome: string;
+  sigla: string;
+}
+export interface PessoaUsuaria {
+  nome: string;
+  nascimento: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  senha: string;
+  cidade: string;
+  estado: UnidadeFederativa;
+}
